43ca386eeb7ac210255d9cc5a0bf4e697d361bec
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-09-04 17:14:05 +0200

    Add built files

---
+++ wp-content/themes/terapirekommendationer/assets/dist/js/app.js
@@ -0,0 +1,27 @@
+var Terapirekommendationer;
+
+Terapirekommendationer = Terapirekommendationer || {};
+Terapirekommendationer.ExampleNamespace = Terapirekommendationer.Liquid || {};
+
+Terapirekommendationer.ExampleNamespace.ExampleClass = (function ($) {
+
+	var classVariable = false;
+
+    /**
+     * Constructor
+     * Should be named as the class itself
+     */
+	function ExampleClass() {
+
+    }
+
+    /**
+     * Method
+     */
+    ExampleClass.prototype.exampleMethod = function () {
+
+    }
+
+	return new ExampleClass();
+
+})(jQuery);
