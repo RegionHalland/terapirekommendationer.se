7e41c1aa3d076fb80f9e586d34623d82efa58abf
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2017-09-11 11:46:00 +0200

    Add browser-sync for automatic style injection and reloading

--- wp-content/themes/terapirekommendationer/gulpfile.js
+++ wp-content/themes/terapirekommendationer/gulpfile.js
@@ -8,6 +8,7 @@ var uglify 			= require('gulp-uglify');
 var cssnano 		= require('gulp-cssnano');
 var rename 			= require('gulp-rename');
 var autoprefixer 	= require('gulp-autoprefixer');
+var browserSync    = require('browser-sync');
 
 // Compile Our Sass
 gulp.task('sass-dist', function() {
@@ -19,7 +20,8 @@ gulp.task('sass-dist', function() {
                 mergeLonghand: false,
                 zindex: false
             }))
-            .pipe(gulp.dest('assets/dist/css'));
+            .pipe(gulp.dest('assets/dist/css'))
+            .pipe(browserSync.stream());
 });
 
 gulp.task('sass-dev', function() {
@@ -27,7 +29,8 @@ gulp.task('sass-dev', function() {
             .pipe(sass())
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
             .pipe(rename({suffix: '.dev'}))
-            .pipe(gulp.dest('assets/dist/css'));
+            .pipe(gulp.dest('assets/dist/css'))
+            .pipe(browserSync.stream());
 });
 
 // Concatenate & Minify JS
@@ -46,6 +49,21 @@ gulp.task('watch', function() {
     gulp.watch('assets/source/sass/**/*.scss', ['sass-dist', 'sass-dev']);
 });
 
+/*gulp.task('bs', function() {
+    browserSync.init({
+        proxy: "tr.app"
+    });
+});*/
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+    browserSync.init({
+        proxy: "tr.app"
+    });
+
+    gulp.watch('assets/source/sass/**/*.scss', ['sass-dist', 'sass-dev']);
+});
+
 // Default Task
-gulp.task('default', ['sass-dist', 'sass-dev', 'scripts-dist', 'watch']);
+gulp.task('default', ['sass-dist', 'sass-dev', 'scripts-dist', 'watch', 'bs']);
 
