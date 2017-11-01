6f734f96698e3be50be43ef4962c06c88a933d5d
David Öhlin <hello@davidohl.in>
2017-11-01 17:01:00 +0100

    add ajax request for icon sprite

--- wp-content/themes/terapirekommendationer/assets/src/js/app.js
+++ wp-content/themes/terapirekommendationer/assets/src/js/app.js
@@ -1,1 +1,10 @@
 var Terapirekommendationer;
+
+(function($) {
+	$.get("https://regionhalland.github.io/styleguide/dist/icons/sprite-2.svg", function(data) {
+		var div = document.createElement("div");
+		div.className = 'no-display';
+		div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+		document.body.insertBefore(div, document.body.childNodes[0]);
+	});
+})( jQuery );
