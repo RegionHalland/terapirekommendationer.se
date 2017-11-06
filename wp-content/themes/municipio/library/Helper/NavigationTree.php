eb0a6764ca41a8adeb122ec02a67b32af8b23dab
David Öhlin <hello@davidohl.in>
2017-11-06 14:36:40 +0100

    get the correct spritesheet and fix arrow

--- wp-content/themes/municipio/library/Helper/NavigationTree.php
+++ wp-content/themes/municipio/library/Helper/NavigationTree.php
@@ -485,7 +485,7 @@ class NavigationTree
         ));
 
         if ($outputSubmenuToggle) {
-            $this->addOutput('<button data-load-submenu="' . $objId . '"><span class="sr-only">' . __('Show submenu', 'municipio') . '<svg class="icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#chevron-bottom"></use></svg></button>');
+            $this->addOutput('<button class="vertical-nav__toggle" data-load-submenu="' . $objId . '"><span class="sr-only">' . __('Show submenu', 'municipio') . '</span><svg class="icon--sm"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#caret-bottom"></use></svg></button>');
         }
     }
 
