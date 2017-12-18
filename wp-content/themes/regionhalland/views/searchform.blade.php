6c83de529cb858021714b315399bff6f410ff5ad
David Öhlin <hello@davidohl.in>
2017-12-18 16:37:21 +0100

    update search example text

--- wp-content/themes/regionhalland/views/searchform.blade.php
+++ wp-content/themes/regionhalland/views/searchform.blade.php
@@ -8,7 +8,7 @@
 					<label class="search-header__label" for="search__input">Skriv för att börja söka i terapirekommendationerna</label>
 					<input id="search-input" class="search-header__input" type="search" placeholder="Skriv här för att börja söka">
 				</div>
-				<span class="search-header__example">Ex. Behandling luftrörskatarr</span>
+				<span class="search-header__example">Ex. Behandling hjärnbristanemi</span>
 				
 			</div>
 		</div>
