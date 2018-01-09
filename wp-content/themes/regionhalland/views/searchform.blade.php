59ca23855ac2f6546ead156196ee3ea8f10c849c
David Öhlin <hello@davidohl.in>
2018-01-09 16:10:23 +0100

    change hjärnbrist to järnbrist 💡

--- wp-content/themes/regionhalland/views/searchform.blade.php
+++ wp-content/themes/regionhalland/views/searchform.blade.php
@@ -8,7 +8,7 @@
 					<label class="search-header__label" for="search__input">Skriv för att börja söka i terapirekommendationerna</label>
 					<input id="search-input" class="search-header__input" type="search" placeholder="Skriv här för att börja söka">
 				</div>
-				<span class="search-header__example">Ex. Behandling hjärnbristanemi</span>
+				<span class="search-header__example">Ex. Behandling järnbristanemi</span>
 				
 			</div>
 		</div>
